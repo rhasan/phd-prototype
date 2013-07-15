c1c653261559b545b9a4d9df2ce4ac32e9c48ce2
Rakebul Hasan <rakeb04@gmail.com>
2013-07-15 18:16:00 +0200

    first commit with dbpedia endpoint test

---
+++ movie-recommender.py
@@ -0,0 +1,23 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+sparql = SPARQLWrapper("http://dbpedia-test.inria.fr/sparql")
+sparql.setQuery("""
+SELECT ?m count(?m) AS ?num
+WHERE
+{
+  ?m dcterms:subject ?o.
+  dbpedia:The_Matrix dcterms:subject ?o.
+  ?m a dbpedia-owl:Film.
+  FILTER (?m != dbpedia:The_Matrix)
+
+}
+
+GROUP BY ?m ORDER BY DESC(?num)
+LIMIT 50
+""")
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+for result in results["results"]["bindings"]:
+    print result["m"]["value"], result["num"]["value"]
+    
