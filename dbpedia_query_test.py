97e2e98e7de401befef68116818d1dd523fc0d68
Rakebul Hasan <rakeb04@gmail.com>
2013-07-16 16:44:32 +0200

    processing movielens dataset added

--- dbpedia_query_test.py
+++ dbpedia_query_test.py
@@ -1,23 +1,25 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
-
+import util
 sparql = SPARQLWrapper("http://dbpedia-test.inria.fr/sparql")
-sparql.setQuery("""
+queryStr = """
 SELECT ?m count(?m) AS ?num
 WHERE
 {
-	dbpedia:The_Matrix dcterms:subject ?o.
+	<http://dbpedia.org/resource/Inception> dcterms:subject ?o.
 	?m dcterms:subject ?o.
 	?m a dbpedia-owl:Film.
-	FILTER (?m != dbpedia:The_Matrix)
+	FILTER (?m != <http://dbpedia.org/resource/Inception>)
 
 }
 
 GROUP BY ?m ORDER BY DESC(?num)
 LIMIT 50
-""")
+""";
+sparql.setQuery(queryStr)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
 for result in results["results"]["bindings"]:
+    #print util.url_decode(result["m"]["value"]), result["num"]["value"]
     print result["m"]["value"], result["num"]["value"]
     
--- movie_recommender.py
+++ movie_recommender.py
@@ -1,10 +1,15 @@
 from datasets import MovieLens
 
-DBP_ML_MAPPING = "data/mapping-movielens-dbpedia.csv"
 
 def main():
 	ml = MovieLens()
-	ml.load_dbpedia_mapping(DBP_ML_MAPPING)
+	ml.load_data()
+	#print ml.get_DBpedia_mapped_movie_list()
+	print ml.get_avg_movie_rating_by_dbpedia_uri('http://dbpedia.org/resource/Roman_Holiday')
+	print ml.get_avg_movie_rating_by_dbpedia_uri('http://dbpedia.org/resource/Wag_the_Dog')
+
+	print ml.get_user_rating_data('122')
+	#print
 
 if __name__ == "__main__":
 	main()
