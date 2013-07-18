f4727d536df642f8fd2fda93a513bf2cc16ca184
Rakebul Hasan <rakeb04@gmail.com>
2013-07-18 12:26:34 +0200

    movie recommendation implementation

--- movie_recommender.py
+++ movie_recommender.py
@@ -1,4 +1,40 @@
+import util
 from datasets import MovieLens
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+DBPEDIA_ENDPOINT = "http://dbpedia-test.inria.fr/sparql"
+
+
+class MovieRecommender:
+	def __init__(self):
+		pass
+
+	def get_movie_recommendations(self,dbpedia_uri):
+
+		sparql = SPARQLWrapper(DBPEDIA_ENDPOINT)
+		queryStr = """
+		SELECT ?m count(?m) AS ?num
+		WHERE
+		{
+			<"""+dbpedia_uri+"""> dcterms:subject ?o.
+			?m dcterms:subject ?o.
+			?m a dbpedia-owl:Film.
+			FILTER (?m != <http://dbpedia.org/resource/Catch_Me_If_You_Can>)
+
+		}
+
+		GROUP BY ?m ORDER BY DESC(?num)
+		LIMIT 50
+		""";
+		sparql.setQuery(queryStr)
+		sparql.setReturnFormat(JSON)
+		results = sparql.query().convert()
+
+		for result in results["results"]["bindings"]:
+		    print util.url_decode(result["m"]["value"]), result["num"]["value"]
+		    #print result["m"]["value"], result["num"]["value"]
+
+
 
 
 def main():
